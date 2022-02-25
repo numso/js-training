e21b63932503dbe3a6b8207f0d5fc31178c9cee9
Dallin Osmun <dosmun@gmail.com>
2022-02-25 08:06:14 -0700

    remove dupe code

--- 04-more-functions.js
+++ 04-more-functions.js
@@ -25,7 +25,6 @@ function random () {
 /////////////
 
 test('random', () => {
-  let fn = random()
   for (let i = 0; i < 1000; ++i) {
     expect(random()).toBeOneOf([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
   }
