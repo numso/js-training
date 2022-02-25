031b0946dd2edd4e8c49ee9fcdcbe136e5b6a9c5
Dallin Osmun <dosmun@gmail.com>
2022-02-25 07:56:05 -0700

    use a more descriptive assertion

--- 04-more-functions.js
+++ 04-more-functions.js
@@ -27,7 +27,7 @@ function random () {
 test('random', () => {
   let fn = random()
   for (let i = 0; i < 1000; ++i) {
-    expect([0, 1, 2, 3, 4, 5, 6, 7, 8, 9].includes(random())).toBe(true)
+    expect(random()).toBeOneOf([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
   }
 })
 
